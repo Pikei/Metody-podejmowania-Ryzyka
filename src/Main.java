4b0b26d1e4f231d2a336dac63f7bc5bbfff42075
Pikej <piotrkarol2601@gmail.com>
2024-02-02 13:51:50 +0100

    Troszkę przerobiona funkcja main żeby było jakieś menu dla tych kodów bardzo podstawowe. Wersja pre alpha

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import hookejeeves.HookeJeeves;
 import idiotoodporne.*;
 import montecarlo.MonteCarlo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Ahh shit. Here we go again...");
-        new MonteCarlo();
-//        new MC();
+        System.out.println("1. Monte Carlo");
+        System.out.println("2. Hooke Jeeves");
+        switch (input.next()) {
+            case "1" -> {
+                new MonteCarlo();
+//                new MC();
+            }
+            case "2" -> {new HookeJeeves();}
+        }
     }
 }
